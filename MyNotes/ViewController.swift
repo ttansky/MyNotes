09d1b7effd0d4d8ad519a22321c0b836a1c46d3c
Taras Tansky <tarastansky@gmail.com>
2019-05-22 12:37:21 +0300

    Initial Commit

---
+++ MyNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyNotes
+//
+//  Created by Тарас on 5/22/19.
+//  Copyright © 2019 Taras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
